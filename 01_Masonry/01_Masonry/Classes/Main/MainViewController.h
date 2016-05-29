0a8076b45a4a9dade495c014fbbeec4e571abb4c
倪新生 <1911398892@qq.com>
2016-05-29 17:34:33 +0800

    1、Masonry示例程序 2、cocoapods引入Masonry   https://github.com/SnapKit/Masonry，CocoaPods引入项目不识别问题 3、info.plist网络 https注意问题 4、全局宏引入，注意路径

---
+++ 01_Masonry/01_Masonry/Classes/Main/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  01_Masonry
+//
+//  Created by nixinsheng on 16/5/29.
+//  Copyright © 2016年 AsiaInfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ 01_Masonry/Pods/Target Support Files/Pods-01_Masonry/Pods-01_Masonry-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_01_MasonryVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_01_MasonryVersionString[];
+
